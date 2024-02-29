cbcd3d8b2fd197e30f65bed5a896535c3b59eef5
masawai <secretariat.tmopera@gmail.com>
2024-02-29 14:13:08 +0900

    fix gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/Footer";
-import { GoogleAnalytics } from "@/components/GoogleAnalytics";
+import { GTM_ID } from "@/utils/gtm";
 import { GeistSans } from "geist/font/sans";
+import Script from "next/script";
 import { Suspense } from "react";
 import "./globals.css";
 
@@ -21,14 +22,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja" className={GeistSans.className}>
-      {/* <head>
+      <head>
         <Suspense>
-          <GoogleAnalyticsScript />
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer', '${GTM_ID}');
+    `,
+            }}
+          />
         </Suspense>
-      </head> */}
+      </head>
       <body className="bg-background text-foreground">
         <Suspense>
-          <GoogleAnalytics />
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
         </Suspense>
         <main className="min-h-screen flex flex-col items-center font-sans">
           {children}
